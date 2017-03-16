3eac25f671d89c615363e8c91b85b88cfd24f8f7
tsoernes <t.soernes@gmail.com>
2017-03-16 20:40:12 +0100

    Finished W7 (ignoring editor)

--- W7/JoinList.hs
+++ W7/JoinList.hs
@@ -43,10 +43,25 @@ main = do
   print $ toString jli
 
   -- Test fromString
-  let scoreSizeJL = fromString "e\n\nf\nab q\nc\nd" :: JoinList (Score, Size) String
+  -- let tString = "e\n\nf\nab q\nc\nd"
+  let tString = "e \n f \n ab q \n c \n d"
+      scoreSizeJL = fromString tString :: JoinList (Score, Size) String
   print scoreSizeJL
 
+  -- Test fromString . toString
+  print tString
   print $ toString scoreSizeJL
+  print $ toString scoreSizeJL == tString
+
+  -- Test line
+  print $ map (`line` scoreSizeJL) [0..6]
+
+  -- Test replaceLine
+
+  -- Test numLines
+
+  -- Test value
+
 
 data JoinList m a = Empty
                   | Single m a
@@ -55,29 +70,23 @@ data JoinList m a = Empty
 
 
 scoreSizeLine :: String -> JoinList (Score, Size) String
-scoreSizeLine str = Single (scoreString str, Size . length $ words str) str
+scoreSizeLine str = Single (scoreString str, Size . length $ lines str) str
+
 
 instance Buffer (JoinList (Score, Size) String) where
   toString Empty                  = ""
   toString (Single (_,_) st)      = st
-  toString (Append (_,_) st1 st2) = toString st1 ++ toString st2
+  toString (Append (_,_) st1 st2) = toString st1 ++ "\n" ++ toString st2
 
-  -- Assume that Size counts the number words.
-  -- Create a Single for each line with the calculated Score and Size
-  -- Then +++ the singles.
-  fromString st = foldr ((+++) . scoreSizeLine) Empty (lines st)
+  fromString "" = Empty
+  fromString st = foldr1 (+++) $ map scoreSizeLine (lines st)
 
   line = indexJ
 
-  -- fromString or create a single manually? Can a lines be replaces with multiple lines?
-  replaceLine n l jl = takeJ (n-1) jl +++ fromString l +++ dropJ n jl
+  replaceLine n l jl = takeJ n jl +++ fromString l +++ dropJ (n+1) jl
 
-  numLines Empty = 0
-  numLines (Single _ _) = 1
-  numLines (Append _ jl1 jl2) = numLines jl1 + numLines jl2
-
-  value = sz
-
+  numLines = sz
+  value = getScore . fst . tag
 
 (+++) :: Monoid m => JoinList m a -> JoinList m a -> JoinList m a
 (+++) a b = Append (t1 `mappend` t2) a b
@@ -85,41 +94,51 @@ instance Buffer (JoinList (Score, Size) String) where
     t1 = tag a
     t2 = tag b
 
+
 tag :: Monoid m => JoinList m a -> m
 tag Empty          = mempty
 tag (Single m _)   = m
 tag (Append m _ _) = m
 
+
 sz :: (Sized m, Monoid m) => JoinList m a -> Int
 sz = getSize . size . tag
 
+
 indexJ :: (Sized b, Monoid b)
-       => Int -> JoinList b a -> Maybe a
+        => Int -> JoinList b a -> Maybe a
+indexJ _ Empty             = Nothing
 indexJ idx a | idx >= sz a = Nothing
+             | idx <  0    = Nothing
 indexJ 0 (Single _ a)      = Just a
-indexJ 0 Empty             = Nothing
 indexJ idx (Append _ a b)  =
   if idx < sz a
     then indexJ idx a
     else indexJ (idx - sz a) b
+indexJ _ (Single _ _) = Nothing -- this should not happen with a properly constructed JoinList
+
 
 dropJ :: (Sized b, Monoid b)
       => Int -> JoinList b a -> JoinList b a
 dropJ n jl | n >= sz jl = Empty
-dropJ 0 jl = jl
-dropJ n (Append _ a b) =
+dropJ 0 jl              = jl
+dropJ n (Append _ a b)  =
   if n < sz a
      then dropJ n a +++ b
      else dropJ (n - sz a) b
+dropJ _ _ = Empty -- p Empty/Single for p != 0
+
 
 takeJ :: (Sized b, Monoid b)
       => Int -> JoinList b a -> JoinList b a
 takeJ n jl | n >= sz jl = jl
-takeJ 0 _ = Empty
+takeJ 0 _               = Empty
 takeJ n (Append _ a b)  =
   if n < sz a
      then takeJ n a
      else a +++ takeJ (n - sz a) b
+takeJ _ _ = Empty -- p Empty/Single for p != 0
+
 
 scoreLine :: String -> JoinList Score String
 scoreLine str = Single (scoreString str) str
@@ -130,6 +149,7 @@ scoreLine str = Single (scoreString str) str
 --   if i < sz a
 --      then jlRec i a -- +++ b
 --      else --- a +++ --- jlRec (n - sz a) b
+
 
 jlToList :: JoinList m a -> [a]
 jlToList Empty            = []
